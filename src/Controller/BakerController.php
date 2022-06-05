70eb78aa80076401dcebe6ef4dcbd0bdfcd03493
EmmanuelleAllain <e.allain13@gmail.com>
2022-06-05 13:43:25 +0200

    set index baker page and list baker page

--- src/Controller/BakerController.php
+++ src/Controller/BakerController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\BakerRepository;
+use App\Entity\Baker;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/baker', name: 'app_baker_')]
 class BakerController extends AbstractController
 {
-    #[Route('/baker', name: 'app_baker')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(BakerRepository $bakerRepository): Response
     {
+        $bakers = $bakerRepository->findAll();
         return $this->render('baker/index.html.twig', [
-            'controller_name' => 'BakerController',
+            'bakers' => $bakers,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'list')]
+    public function detail(Baker $baker): Response
+    {
+        return $this->render('baker/show.html.twig', [
+            'baker' => $baker,
         ]);
     }
 }
