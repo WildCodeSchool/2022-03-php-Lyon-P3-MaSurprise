64129cd04b43b349f0e69cdd237fe21db0687b0f
EmmanuelleAllain <e.allain13@gmail.com>
2022-06-05 12:59:06 +0200

    rmove userController and add BakerController

--- src/Controller/BakerController.php
+++ src/Controller/BakerController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserController extends AbstractController
+class BakerController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/baker', name: 'app_baker')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        return $this->render('baker/index.html.twig', [
+            'controller_name' => 'BakerController',
         ]);
     }
 }
