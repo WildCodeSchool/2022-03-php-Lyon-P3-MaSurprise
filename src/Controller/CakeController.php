7ab7e263c6796098fd5ce9fee08f828ba9ce4ec4
dldvlpr <dormoy.dev@gmail.com>
2022-06-07 17:27:17 +0200

    add cake controller and cake view. we can show list of cake and detail of one

--- src/Controller/CakeController.php
+++ src/Controller/CakeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Cake;
+use App\Repository\CakeRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class CakeController extends AbstractController
 {
     #[Route('/cake', name: 'app_cake')]
-    public function index(): Response
+    public function index(CakeRepository $cakeRepository): Response
     {
+        $cakes = $cakeRepository->findAll();
         return $this->render('cake/index.html.twig', [
-            'controller_name' => 'CakeController',
+            'cakes' => $cakes,
+        ]);
+    }
+
+    #[Route('/cake/{id}/', name: 'app_cake_show')]
+    public function show(Cake $cake): Response
+    {
+
+        return $this->render('cake/show.html.twig', [
+            'cake' => $cake,
         ]);
     }
 }
