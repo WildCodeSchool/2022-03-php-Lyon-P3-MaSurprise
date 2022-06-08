88eb0d121aa366adcf73c7ceedc9cd1b6a600801
EmmanuelleAllain <e.allain13@gmail.com>
2022-06-08 20:43:39 +0200

    add better faker data in fixtures

--- src/DataFixtures/BakerFixtures.php
+++ src/DataFixtures/BakerFixtures.php
@@ -11,12 +11,15 @@ class BakerFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create();
-        for ($i = 0; $i < 10; $i++) {
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 15; $i++) {
             $baker = new Baker();
             $baker->setCreated($faker->dateTime);
             $baker->setLastname($faker->lastName());
             $baker->setFirstname($faker->firstName());
+            $baker->setCommercialname($faker->optional()->randomElement(
+                ['La fée des gâteaux', 'Super Cake', 'Maison Truffe', 'Cakery Lina', 'Chef Alex', 'Gâteau sur commande']
+            ));
             $baker->setEmail($faker->email());
             $baker->setPassword($faker->password(8, 15));
             $baker->setPhone($faker->phoneNumber());
