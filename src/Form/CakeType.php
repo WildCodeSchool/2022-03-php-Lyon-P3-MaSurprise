39143ee2206f6b760eb770c0a76c72dfa67863be
Nicolas Girardet <nicolasgirardet@live.fr>
2022-06-08 10:13:58 +0200

    translated the labels

--- src/Form/CakeType.php
+++ src/Form/CakeType.php
@@ -14,17 +14,17 @@ class CakeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('picture1')
-            ->add('picture2')
-            ->add('picture3')
-            ->add('picture4')
-            ->add('picture5')
-            ->add('description', TextType::class)
-            ->add('allergens', TextType::class)
-            ->add('price', NumberType::class)
-            ->add('size', TextType::class)
-            ->add('baker', null, ['choice_label' => 'fullname'])
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('picture1', TextType::class, ['label' => 'Image 1'])
+            ->add('picture2', TextType::class, ['label' => 'Image 2'])
+            ->add('picture3', TextType::class, ['label' => 'Image 3'])
+            ->add('picture4', TextType::class, ['label' => 'Image 4'])
+            ->add('picture5', TextType::class, ['label' => 'Image 5'])
+            ->add('description', TextType::class, ['label' => 'Description'])
+            ->add('allergens', TextType::class, ['label' => 'Liste des allergènes'])
+            ->add('price', NumberType::class, ['label' => 'Prix'])
+            ->add('size', TextType::class, ['label' => 'Taille'])
+            ->add('baker', null, ['label' => 'Pâtissier', 'choice_label' => 'fullname'])
             ;
     }
 
