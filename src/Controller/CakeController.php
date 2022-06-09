e354f89740028ea809504863404c93e5b51d5a0c
dldvlpr <dormoy.dev@gmail.com>
2022-06-09 12:22:02 +0200

    erase and add use exception

--- src/Controller/CakeController.php
+++ src/Controller/CakeController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use _PHPStan_0a43b4828\Nette\Neon\Exception;
+use Exception;
 use App\Entity\Cake;
-use App\Repository\CakeRepository;
 use App\Form\CakeType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CakeRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/cake')]
 class CakeController extends AbstractController
