a5403928dc7e65f2a5e06c9f7ef647f21ffcac26
Celie Russier <celie.russier@gmail.com>
2022-06-15 15:29:54 +0200

    authentication with registration controller and form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -32,7 +32,18 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password']
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
         ;
     }
