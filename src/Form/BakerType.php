a1fb5c2e7d2c432c5a6f51696255d6422825e78d
EmmanuelleAllain <e.allain13@gmail.com>
2022-06-16 09:52:47 +0200

    baker form

--- src/Form/BakerType.php
+++ src/Form/BakerType.php
@@ -4,46 +4,96 @@ namespace App\Form;
 
 use App\Entity\Baker;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class BakerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('bakerType', ChoiceType::class, [
+                'label' => 'Vous êtes :*',
+                'choices' => ['professionnel' => 'professionnel', 'amateur' => 'amateur'],
+                'expanded' => true,
+                'multiple' => true,
+                ])
+            ->add('services', ChoiceType::class, [
+                'label' => 'Quelle(s) prestation(s) proposez-vous ?*',
+                'choices' => ['pâtissier' => 'pâtissier', 'atelier' => 'atelier', 'formation' => 'formation'],
+                'expanded' => true,
+                'multiple' => true,
+                ])
             ->add('lastname', TextType::class, [
-            'label' => 'Nom',
-            'required' => 'Le champ nom est obligatoire'
+            'label' => 'Nom*',
+            'required' => 'Le champ Nom est obligatoire'
             ])
             ->add('firstname', TextType::class, [
-                'label' => 'Prénom',
-                'required' => 'Le champ prénom est obligatoire'
+                'label' => 'Prénom*',
+                'required' => 'Le champ Prénom est obligatoire'
                 ])
             ->add('commercialName', TextType::class, [
                 'label' => "Nom de l'entreprise",
-                'required' => "Le champ Nom de l'entreprise est obligatoire"
+                'required' => false
                 ])
+            ->add('logoFile', VichFileType::class, [
+                'label' => 'Logo',
+                'required' => false,
+                'allow_delete' => false,
+                'download_uri' => true,
+            ])
+            ->add('siretFile', VichFileType::class, [
+                'label' => 'Siret',
+                'required' => false,
+                'allow_delete' => false,
+                'download_uri' => true,
+            ])
+            ->add('diplomaFile', VichFileType::class, [
+                'label' => 'CAP, autre diplôme ou accréditation',
+                'required' => false,
+                'allow_delete' => false,
+                'download_uri' => true,
+            ])
             ->add('email', EmailType::class, [
-                'label' => "E-mail",
+                'label' => "E-mail*",
                 'required' => "Le champ E-mail est obligatoire"
                 ])
             ->add('password', TextType::class, [
-                'label' => "Mot de passe",
+                'label' => "Mot de passe*",
                 'required' => "Le champ Mot de passe est obligatoire"
                 ])
             ->add('address', TextareaType::class, [
-                'label' => "Adresse",
+                'label' => "Adresse*",
                 'required' => "Le champ Adresse est obligatoire"
                 ])
             ->add('phone', TextType::class, [
-                'label' => "Téléphone",
-                'required' => "Le champ Nom de l'entreprise est obligatoire"
+                'label' => "Téléphone*",
+                'required' => "Le champ Téléphone est obligatoire"
+                ])
+            ->add('facebook', TextType::class, [
+                'label' => "Facebook",
+                ])
+            ->add('instagram', TextType::class, [
+                'label' => "Instagram",
                 ])
         ;
+        $builder->get('services')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($servicesAsString) {
+                    // transform the string back to an array
+                    return explode(', ', $servicesAsString);
+                },
+                function ($servicesAsArray) {
+                    // transform the array to a string
+                    return implode(', ', $servicesAsArray);
+                },
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
