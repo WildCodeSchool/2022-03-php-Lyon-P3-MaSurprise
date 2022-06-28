07e021729c610ae8e4df5f3a9e4cbd73278c7592
EmmanuelleAllain <e.allain13@gmail.com>
2022-06-28 09:42:55 +0200

    remove useless use

--- src/Controller/CakeController.php
+++ src/Controller/CakeController.php
@@ -8,15 +8,12 @@ use Exception;
 use App\Entity\Cake;
 use App\Repository\CakeRepository;
 use App\Service\UploaderHelper as ServiceUploaderHelper;
-use GrumPHP\Task\Robo;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\Serializer\Normalizer\DataUriNormalizer;
-use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 #[Route('/gateau', name: 'app_cake_')]
 class CakeController extends AbstractController
