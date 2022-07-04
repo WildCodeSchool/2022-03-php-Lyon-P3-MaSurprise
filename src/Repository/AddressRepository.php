f8222fe2a5efcce993a78c227e9ac413fb45dbec
ghartemann <g.hartemann@gmail.com>
2022-07-04 17:00:25 +0200

    removed dead code

--- src/Repository/AddressRepository.php
+++ src/Repository/AddressRepository.php
@@ -39,44 +39,4 @@ class AddressRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    // fetching addresses whose status match searched status
-//    public function findLikeStatus(User $user, mixed $status): mixed
-//    {
-//        $queryBuilder = $this->createQueryBuilder('a')
-//            ->join('a.user', 'u')
-//            ->addSelect('u')
-//            ->where('a.status LIKE :status')
-//            ->andWhere('a.billingAddress LIKE :user')
-//            ->setParameter('status', $status)
-//            ->setParameter('billingAddress', $user)
-//            ->getQuery();
-//
-//        return $queryBuilder->getResult();
-//    }
-
-//    /**
-//     * @return Address[] Returns an array of Address objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Address
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
