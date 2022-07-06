126fb63b0dde07f5fbb2203546b18a6588c4461d
ghartemann <g.hartemann@gmail.com>
2022-07-06 17:25:51 +0200

    a lot of work has been achieved today

--- migrations/Version20220706140925.php
+++ migrations/Version20220706140925.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220706085543 extends AbstractMigration
+final class Version20220706140925 extends AbstractMigration
 {
     public function getDescription(): string
     {
