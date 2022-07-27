349752d7e365847ab4b0dec4af8a0ee63cd01259
dldvlpr <dormoy.dev@gmail.com>
2022-07-27 14:04:35 +0200

    fix date

--- assets/scripts/dateverification.js
+++ assets/scripts/dateverification.js
@@ -1,2 +1,2 @@
 const date = new Date().toISOString().slice(0,10)
-document.getElementById('meeting-time').setAttribute("min", date);
+document.getElementById('meetingTime').setAttribute("min", date);
